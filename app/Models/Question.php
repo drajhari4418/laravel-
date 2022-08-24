ee5af70e80322cc3ecfc07df2b27612a6771ce37
drajhari4418 <dushyantkaushik12@gmail.com>
2022-08-24 22:47:51 +0530

    slug entry through tinker

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -7,5 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    use HasFactory;
+    protected $fillable = ['title','body'];
+    
+   public function user()
+   {
+    return $this->belongsTo(User::class);
+   }
 }
--- database/migrations/2022_08_23_180639_create_questions_table.php
+++ database/migrations/2022_08_23_180639_create_questions_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Ramsey\Uuid\Type\Integer;
 
 return new class extends Migration
 {
@@ -14,8 +15,18 @@ return new class extends Migration
     public function up()
     {
         Schema::create('questions', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('body');
+            $table->unsignedInteger('views')->default(0);
+            $table->unsignedInteger('answers')->default(0);
+            $table->Integer('votes')->default(0);
+            $table->unsignedInteger('best_answer_id')->nullable();
+            $table->unsignedBigInteger('user_id');
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
